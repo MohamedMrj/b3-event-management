03b1dac0171e85a3b521524a48699c6580c8b324
Sebastian <sebastian.danielsson@proton.me>
2024-01-24 01:22:22 +0100

    Make components standalone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,19 @@ import { NgModule } from "@angular/core";
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from './header/header.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FooterComponent } from './footer/footer.component';
+import { EventCardComponent } from './event-card/event-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatCheckboxModule
+    HeaderComponent,
+    FooterComponent,
+    EventCardComponent,
   ],
   bootstrap: [
     AppComponent
