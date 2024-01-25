9e268cf126aece0fb137cc17c20306fd7cb0e320
Sebastian <sebastian.danielsson@proton.me>
2024-01-25 13:06:31 +0100

    Css grid for easier responsive design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -56,6 +56,21 @@ export class AppComponent implements OnInit {
       eventImageUrl: `https://www.jacksonsart.com/blog/wp-content/uploads/2020/01/Mall-Gallery-Main-Gallery-Exhibition-Hire.jpg`,
       eventImageAlt: 'Photo of an art exhibition',
     },
-    
+    {
+      id: 3,
+      title: 'Shiba Inu Gathering',
+      subtitle: 'Dog Lovers Meetup',
+      longText: 'Join us for a fun afternoon with Shiba Inus!',
+      eventImageUrl: `https://material.angular.io/assets/img/examples/shiba2.jpg`,
+      eventImageAlt: 'Photo of a Shiba Inu',
+    },
+    {
+      id: 4,
+      title: 'Tech Conference',
+      subtitle: 'Exploring the Latest Technologies',
+      longText: 'Join us for a conference to learn about the latest technologies and trends in the industry.',
+      eventImageUrl: `https://assets-global.website-files.com/6331e19fdfcbe01f4c12b610/640f82ab5d300300a891d92a_viva.jpeg`,
+      eventImageAlt: 'Photo of a tech conference',
+    }
   ];
 }
