b05b72d44c946a4c4ee1b753501f5c3aad86861b
Sebastian <sebastian.danielsson@proton.me>
2024-02-01 17:05:29 +0100

    Add confirmation dialog when returning from form

---
+++ src/app/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,30 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  template: `
+    <h1 mat-dialog-title>Confirm</h1>
+    <div mat-dialog-content>
+      <p>{{ data.message }}</p>
+    </div>
+    <div mat-dialog-actions>
+      <button mat-button (click)="onDismiss()">Cancel</button>
+      <button mat-button (click)="onConfirm()">Discard</button>
+    </div>
+  `,
+})
+export class ConfirmDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  onConfirm(): void {
+    this.dialogRef.close(true);
+  }
+
+  onDismiss(): void {
+    this.dialogRef.close(false);
+  }
+}
--- src/app/event-create/event-create.component.ts
+++ src/app/event-create/event-create.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Location } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-event-create',
@@ -11,11 +13,24 @@ export class EventCreateComponent {
   constructor(
     private titleService: Title,
     private pageLocation: Location,
+    private dialog: MatDialog,
     ) {
     this.titleService.setTitle('Create Event');
   }
 
   goBack() {
     this.pageLocation.back();
+  }
+
+  confirmDiscardChanges() {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: { message: 'Do you want to discard the changes?' }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.goBack();
+      }
+    });
   }
 }
