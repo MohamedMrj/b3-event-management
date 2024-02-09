954e93cd6129d672e9c04c9708b7330f1b5fc973
Emelie Persson <emelie.alltek@hotmail.com>
2024-02-09 21:24:32 +0100

    wip:login page and my event

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,9 @@ import { EventDetailComponent } from './event-detail/event-detail.component';
 import { EventCreateComponent } from './event-create/event-create.component';
 import { EventEditComponent } from './event-edit/event-edit.component';
 import { ApiTestComponent } from './api-test/api-test.component';
+import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MyEventsComponent } from './my-events/my-events.component';
 
 export const routes: Routes = [
     { path: '', component: EventListComponent },
@@ -12,5 +14,7 @@ export const routes: Routes = [
     { path: 'event/update/:eventid', component: EventEditComponent },
     { path: 'event/:eventid', component: EventDetailComponent },
     { path: 'api-test', component: ApiTestComponent },
+    { path: 'login', component: LoginComponent},
+    { path: 'my-events', component: MyEventsComponent},
     { path: '**', component: PageNotFoundComponent },
 ];
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  styleUrls: ['./login.component.css'],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatIconModule, MatButtonModule],
+  templateUrl: './login.component.html'
+})
+export class LoginComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  hide = true;
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+}
