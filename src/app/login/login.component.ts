651094851fa5a0974aa2c7e76c6f75217e798054
Sebastian <sebastian.danielsson@proton.me>
2024-02-14 20:28:34 +0100

    Fix linting warnings. Show Snackbar on error

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,19 +2,20 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-
+import { LoginInfo, LoginResponse } from '../login';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  styleUrls: ['./login.component.css'],
   standalone: true,
-  imports: [MatCardModule, MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatIconModule, MatButtonModule]
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatIconModule]
 })
 export class LoginComponent {
   email: string = '';
@@ -23,41 +24,35 @@ export class LoginComponent {
 
   constructor(
     private http: HttpClient,
-    private router: Router
+    private router: Router,
+    private snackBar: MatSnackBar,
   ) { }
 
   onLoginClick(): void {
-    const loginInfo = {
+    const loginInfo: LoginInfo = {
       email: this.email,
       password: this.password
     };
 
-    this.http.post<any>('api/login', loginInfo)
-      .subscribe(
-        response => {
-          
-          console.log('Response:', response);
+    this.http.post<LoginResponse>('api/login', loginInfo).subscribe({
+      next: (response) => {
+        console.log('Response:', response);
+        if (response.token) {
+          console.log('Token:', response.token);
           localStorage.setItem('token', response.token);
-          
-          // Check if the response contains a token
-          if (response && response.token) {
-            // Log the token to the console
-            console.log('Token:', response.token);
-            
-            // Store token in local storage
-            localStorage.setItem('token', response.token);
-
-            // Redirect to desired route (e.g., dashboard)
-            this.router.navigate(['event']);
-          } else {
-            console.error('Invalid response format:', response);
-            this.loginError = true;
-          }
-        },
-        error => {
-          console.error('Login failed:', error);
+          this.router.navigate(['event']);
+        } else {
+          console.error('Invalid response format:', response);
           this.loginError = true;
         }
-      );
+      },
+      error: (error) => {
+        console.error('Login failed', error);
+        this.snackBar.open(error.error, 'Stäng', {
+          duration: 5000,
+        });
+        this.loginError = true;
+      }
+    });
   }
 }
