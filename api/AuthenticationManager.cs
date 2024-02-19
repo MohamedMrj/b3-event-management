509fade3795c7e7a7eb91bc687bf89761f91ed13
Sebastian <sebastian.danielsson@proton.me>
2024-02-19 17:22:32 +0100

    Revert "Brutal troubleshooting xP"
    This reverts commit 2fb15805827956c2662481903f7082a9c842307d.

--- api/AuthenticationManager.cs
+++ api/AuthenticationManager.cs
@@ -133,50 +133,14 @@ namespace B3.Complete.Eventwebb
                 return new BadRequestObjectResult(new { error = "Missing Authorization header." });
 
             var token = req.Headers["Authorization"].ToString().Replace("Bearer ", "");
-            try
+            var principal = ValidateJwtToken(token);
+            if (principal == null)
             {
-                var principal = ValidateJwtToken(token);
-                if (principal == null)
-                {
-                    return new BadRequestObjectResult(new { error = "Invalid token." });
-                }
+                return new BadRequestObjectResult(new { error = "Invalid token." });
+            }
 
-                return new OkObjectResult(new { valid = true, message = "Token is valid." });
-            }
-            catch (SecurityTokenExpiredException ex)
-            {
-                return new BadRequestObjectResult(new
-                {
-                    error = "Invalid token.",
-                    reason = "Token expired.",
-                    serverTime = DateTime.UtcNow,
-                    validFrom = ex.NotBefore,
-                    validTo = ex.Expires,
-                    secretKey = secretKey, // Extremely sensitive, remove as soon as possible
-                });
-            }
-            catch (SecurityTokenException ex)
-            {
-                return new BadRequestObjectResult(new
-                {
-                    error = "Invalid token.",
-                    reason = ex.Message,
-                    serverTime = DateTime.UtcNow,
-                    secretKey = secretKey, // Extremely sensitive, remove as soon as possible
-                });
-            }
-            catch (Exception ex)
-            {
-                // General catch block for any other unexpected errors
-                return new BadRequestObjectResult(new
-                {
-                    error = "Invalid token.",
-                    reason = "General error.",
-                    errorMessage = ex.Message,
-                    serverTime = DateTime.UtcNow,
-                    secretKey = secretKey, // Extremely sensitive, remove as soon as possible
-                });
-            }
+            // Return a JSON response
+            return new OkObjectResult(new { valid = true, message = "Token is valid." });
         }
 
         private static bool VerifyPasswordHash(string password, string storedHash, string storedSalt)
